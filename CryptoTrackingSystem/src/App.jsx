2d73064e587532c73f85816d878a831fc49a047f
tuharhirawat <tusharhirawat1918@gmail.com>
2024-12-03 23:54:45 +0530

    navbar is created and just welcome message is displayed

--- CryptoTrackingSystem/src/App.jsx
+++ CryptoTrackingSystem/src/App.jsx
@@ -1,24 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './Components/Header'
-import Main from './Components/MainBody'
-import Footer from './Components/footer'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import Header from './Components/Header'
+// import Main from './Components/MainBody'
+// import Footer from './Components/footer'
 
 
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+    
+//       <Header/>
+//       <Main/>
+//       <Footer/>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+import React from 'react';
+import Navbar from './Components/Navbar';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    
-      <Header/>
-      <Main/>
-      <Footer/>
+    <div className="App">
+      <Navbar />
+      <main>
+        <h1>Welcome to the Crypto World</h1>
+        <p>Here You Will Find All Kind Of Crypto Airdrops Whcih Are Free As Well As Paid <br/>
+        Crypto Is The Future. Our Only Motto Is Learn And Earn Crypto</p>        
+      </main>
+    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ CryptoTrackingSystem/src/Components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="WebName">
+          {/* <img src="https://cryptologos.cc/logos/coinstats-logo.png" alt="CoinStats" className="logo-img" /> */}
+          Crypto Tracker
+        </div>
+        <div className="navbar-links">
+          <a href="/" className="nav-link">Home</a>
+          <a href="/portfolio" className="nav-link">Portfolio</a>
+          <a href="/assets" className="nav-link">Assets</a>
+          <a href="/learn" className="nav-link">Learn</a>
+          <a href="/pricing" className="nav-link">Pricing</a>
+        </div>
+        <div className="navbar-buttons">
+          <button className="signin-btn">Sign In</button>
+          <button className="signup-btn">Sign Up</button>
+        </div>
+      </div>
+    </nav>
+    </>
+  );
+}
+
+export default Navbar;
